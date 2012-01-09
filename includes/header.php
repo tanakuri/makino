225db109d83a9bbdc78be7afcc413087acd06b70
kurifu <tanakuri@gmail.com>
2012-01-09 12:29:59 -0800

    description and keywords

--- includes/header.php
+++ includes/header.php
@@ -5,8 +5,8 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
-		<meta name="keywords" content="Yo, Makino, Freelance journalist, Japanese, journalist, 日本語, ジャーナリズム, 経済, 牧野, 洋, 牧野洋">
-		<meta name="description" content="">
+		<meta name="keywords" content="牧野洋, 牧野, 牧野 洋, まきのよう, まきの・よう, マキノ　ヨウ, マキノ・ヨウ, 官報複合体, ジャーナリズム, ジャーナリスト, ジャーナリズムスクール, マスコミ, メディア, 報道, 経済, Ｍ＆Ａ, ドラッカー, バフェット, クレアモント, 日経, Yo Makino, Yo, Makino, Japanese journalist">
+		<meta name="description" content="牧野洋の公式ウェブサイト。著書・訳書、プロフィール、予定、連絡先などを掲載。">
 		<meta name="author" content="Yo Makino">
 		<meta name="copyright" content="January 2012">
 		<title>Yo Makino - <?php echo $title; ?></title>
